784d4c7e54a27a516903bb75e99588d0b4000ae6
bslater509 <bslater509@gmail.com>
2025-08-15 10:33:19 +0100

    Last Sync: 2025-08-15 10:33 (Mobile)

--- Affine/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-bashio::log.info "Starting AFFiNE..."
-
-# Create data directory inside HA config path
-mkdir -p /config/affine-data
-
-# Start AFFiNE, pointing to config storage
-exec yarn start
